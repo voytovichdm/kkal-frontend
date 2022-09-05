1c3f0d38495cadf2ebc291ee5503a96119d7dc9e
Dmytro <voitovych.dm.m@gmail.com>
2022-09-05 20:55:47 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {BrowserRouter as Router} from 'react-router-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import Routes from 'pages/routes'
+import Topbar from 'components/TopBar/topbar'
+import {CurrentUserProvider} from 'contexts/currentUser'
+import CurrentUserChecker from 'components/CurrentUserChecker'
+import './node_modules/styles/style.css'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = () => {
+    return (
+        <CurrentUserProvider>
+            <CurrentUserChecker>
+                <Router>
+                    <Topbar />
+                    <Routes />
+                </Router>
+            </CurrentUserChecker>
+        </CurrentUserProvider>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
